15fc41dc09d45ec747e40d5d511e9efaa5976898
PeachyBuffalo <89888292+PeachyBuffalo@users.noreply.github.com>
2023-11-29 11:14:38 -0500

    Adding an email sender

---
+++ Email_Sender/app_pass.py
@@ -0,0 +1,4 @@
+#ask for password as input
+email_sender = input('Enter your email: ')
+print(email_sender)
+password = input('Enter your password: ')
---
+++ Email_Sender/email_app.py
@@ -0,0 +1,29 @@
+from email.message import EmailMessage
+from app_pass import password, email_sender
+import ssl
+import smtplib
+
+
+email_sender = ''
+email_password = password
+
+email_reciever = 'terey95673@notedns.com'
+
+subject = 'Test Email'
+
+body = 'This is a test email'
+
+msg = EmailMessage()
+msg['From'] = email_sender
+msg['To'] = email_reciever
+msg['Subject'] = subject
+msg.set_content(body)
+
+context = ssl.create_default_context()
+
+#Would be better to use an API now, but this is the quick and dirty way
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_reciever, msg.as_string())
+    print('You did it! ...Email sent successfully')
+
